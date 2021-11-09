943d542af1b9bab01f688341704e50b90565b603
wolff lobo <wolfflobo2@gmail.com>
2021-11-09 11:15:59 -0300

    First Commit

---
+++ src/app/filmes/filmes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-filmes',
+  templateUrl: './filmes.component.html',
+  styleUrls: ['./filmes.component.sass']
+})
+export class FilmesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
