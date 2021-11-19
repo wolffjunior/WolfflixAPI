f0da96ebbbe64de338e1a98da1eef8c3f2579b21
wolff lobo <wolfflobo2@gmail.com>
2021-11-18 23:43:32 -0300

    Add nav-bar and new feats

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FilmesComponent } from './filmes/filmes.component';
+import { HomeComponent } from './home/home.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"", 
+    children:[
+      {
+        path: "popular",
+        component: FilmesComponent
+      },
+      {
+        path: "home",
+        component: HomeComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
