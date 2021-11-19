875e4488e765302288819e7ca3c57f38d571efcd
wolff lobo <wolfflobo2@gmail.com>
2021-11-18 23:56:06 -0300

    Add lasts routes for movie and series

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FilmesComponent } from './filmes/filmes.component';
+import { MovieDetailsComponent } from './filmes/movie-details/movie-details.component';
 import { HomeComponent } from './home/home.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { SearchComponent } from './search/search.component';
+import { SeriesDetailsComponent } from './series/series-details/series-details.component';
+import { SeriesComponent } from './series/series.component';
 
 const routes: Routes = [
   {
@@ -15,6 +19,22 @@ const routes: Routes = [
       {
         path: "home",
         component: HomeComponent
+      },
+      {
+        path: "search",
+        component: SearchComponent
+      },
+      {
+        path: "series",
+        component: SeriesComponent
+      },
+      {
+        path: "filmes/movie-details/:id",
+        component: MovieDetailsComponent
+      },
+      {
+        path: "series/series-details/:id",
+        component: SeriesDetailsComponent
       }
     ]
   }
